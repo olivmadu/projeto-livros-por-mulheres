59679828d419bc8eb34c0b4b6252993f11f7b65c
olivmadu <meduardalinda2009@hotmail.com>
2024-11-17 12:14:45 -0300

    nova

--- js/autoras.js
+++ js/autoras.js
@@ -3,21 +3,21 @@ const autoras = [
         tipo: "autoras",
         titulo: "Clarice Lispector",
         descricao: "Uma das escritoras mais importantes do Brasil, conhecida por sua prosa introspectiva.",
-        imagem: "../imagens/clarice.jpg",
+        imagem: "imagens/clarice.jpg",
         link: "https://pt.wikipedia.org/wiki/Clarice_Lispector"
     },
     {
         tipo: "autoras",
         titulo: "Virginia Woolf",
         descricao: "Escritora inglesa, pioneira na técnica do fluxo de consciência e uma figura do modernismo.",
-        imagem: "../imagens/virginia.jpg",
+        imagem: "imagens/virginia.jpg",
         link: "https://pt.wikipedia.org/wiki/Virginia_Woolf"
     },
     {
         tipo: "autoras",
         titulo: "Simone de Beauvoir",
         descricao: "Filósofa e escritora francesa, conhecida por sua obra 'O Segundo Sexo'.",
-        imagem: "../imagens/simone.jpg",
+        imagem: "imagens/simone.jpg",
         link: "https://pt.wikipedia.org/wiki/Simone_de_Beauvoir"
     },
 ];
